fcf4f545fc5ab5b481db49969a2dc3a0f78ce122
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-04 00:44:53 +0100

    all passed test

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,39 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+print(badge_maker("Moses"))
+
+
+
+names = ["Moses", "Jide", "Alfred"]
 
 def batch_badge_creator(names):
-    return None
+    def badge(name):
+        return f"Hello, my name is {name}."
+        
+    badges = [badge(name) for name in names]
+    return badges
+    
+print(batch_badge_creator(names))
+   
+   
 
 def assign_rooms(names):
-    return None
+    
+    def badge_room(name):
+        index = names.index(name)
+        return f"Hello, {name}! You'll be assigned to room {index + 1}!"
+    return [badge_room(name) for name in names]
+    
+print(assign_rooms(names))      
+    
+    
 
 def printer(names):
-    return None
+    for name in batch_badge_creator(names):
+        print(name)
+        
+    for name in assign_rooms(names):
+        print(name)
+    
+    
+print(printer(names))
